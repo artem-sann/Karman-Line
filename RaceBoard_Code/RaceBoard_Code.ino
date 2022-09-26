a556b6908c38973cd8ea659eb120e73eeea077ca
Artem Sannikov <artemksannikov@gmail.com>
2022-09-26 15:01:36 +0300

    Test Libraries

--- RaceBoard_Code/RaceBoard_Code.ino
+++ RaceBoard_Code/RaceBoard_Code.ino
@@ -1,3 +1,13 @@
+#include <GPS.h>
+#include <ADC.h>
+#include <IntPin.h>
+#include <Macros.h>
+#include <SDCustom.h>
+#include <TelemetryLLCC68.h>
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
