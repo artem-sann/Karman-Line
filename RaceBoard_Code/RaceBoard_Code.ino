8f3d04e934fa62bda7ef544d37591d1ea80917ec
Artem Sannikov <artemksannikov@gmail.com>
2022-09-26 16:14:01 +0300

    Test serial GPS

--- RaceBoard_Code/RaceBoard_Code.ino
+++ RaceBoard_Code/RaceBoard_Code.ino
@@ -7,23 +7,23 @@
 
 //----------------------Переменные для GPS------------------------------------->
 bool GPS_Flag;  //Актуальность данных (true-данные новые и не считывались)
-double Long;  //Долгота, в градусах 
-double Lati;  //Широта, в градусах
-double Sp;  //Горизонтальная скорость, в метрах в секунду 
-double Alt;  //ВысотаБ в метрах в секунду
-String Coord; //Строка с координатами и высотой
+double Long;    //Долгота, в градусах
+double Lati;    //Широта, в градусах
+double Sp;      //Горизонтальная скорость, в метрах в секунду
+double Alt;     //ВысотаБ в метрах в секунду
+String Coord;   //Строка с координатами и высотой
 //----------------------------------------------------------------------------->
 
-bool GPS_piling(double *Long, double  *Lati, double *Sp, double *Alt, String *Coord) {
+bool GPS_piling(double *Long, double *Lati, double *Sp, double *Alt, String *Coord) {
   GPS_m GPS(9600);
   GPS.StartTrack();
 
-  GPS_Flag=GPS.IsUpdated();
-  *Long=GPS.Longitude();
-  *Lati=GPS.Latitude();
-  *Sp=GPS.Speed();
-  *Alt=GPS.Altitude();
-  *Coord=GPS.LonLatAlt();
+  GPS_Flag = GPS.IsUpdated();
+  *Long = GPS.Longitude();
+  *Lati = GPS.Latitude();
+  *Sp = GPS.Speed();
+  *Alt = GPS.Altitude();
+  *Coord = GPS.LonLatAlt();
 
   GPS.StopTrack();
   return GPS_Flag;
@@ -31,17 +31,24 @@ bool GPS_piling(double *Long, double  *Lati, double *Sp, double *Alt, String *Co
 
 
 void setup() {
-  
-
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+
   GPS_Flag = GPS_piling(&Long, &Lati, &Sp, &Alt, &Coord);
-  
-                     
 
-  
-  
 
+
+  Serial.println("\nSpeed \tLongitude \tLatitude \tAltitude");
+  Serial.println("--------------------------------------------------------------");
+  Serial.print(Sp);
+  Serial.print("\t");
+  Serial.print(Long);
+  Serial.print("\t\t");
+  Serial.print(Lati);
+  Serial.print("\t\t");
+  Serial.println(Alt);
+
+  delay(1000);
 }
