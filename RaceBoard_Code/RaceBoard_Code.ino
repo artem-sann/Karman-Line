8489f883621a549d47f3aa5c70d0753fe0ff3756
Artem Sannikov <artemksannikov@gmail.com>
2022-09-26 16:59:29 +0300

    Test serial GPS

--- RaceBoard_Code/RaceBoard_Code.ino
+++ RaceBoard_Code/RaceBoard_Code.ino
@@ -5,15 +5,16 @@
 #include <SDCustom.h>
 #include <TelemetryLLCC68.h>
 
-//----------------------Переменные для GPS------------------------------------->
+//----------------------Переменные для GPS------------------------------------------------->
 bool GPS_Flag;  //Актуальность данных (true-данные новые и не считывались)
 double Long;    //Долгота, в градусах
 double Lati;    //Широта, в градусах
 double Sp;      //Горизонтальная скорость, в метрах в секунду
 double Alt;     //ВысотаБ в метрах в секунду
 String Coord;   //Строка с координатами и высотой
-//----------------------------------------------------------------------------->
+//----------------------------------------------------------------------------------------->
 
+//-----------------------------Функции по работе с GPS------------------------------------->
 bool GPS_piling(double *Long, double *Lati, double *Sp, double *Alt, String *Coord) {
   GPS_m GPS(9600);
   GPS.StartTrack();
@@ -30,6 +31,25 @@ bool GPS_piling(double *Long, double *Lati, double *Sp, double *Alt, String *Coo
 }
 
 
+void print_GPS(double *Long, double *Lati, double *Sp, double *Alt, String *Coord, bool *GPS_Flag) {
+
+  Serial.println("\nSpeed \tLongitude \tLatitude \tAltitude \tCoord \t\t\t\tGPS_Flag");
+  Serial.println("---------------------------------------------------------------------------------------------------");
+  Serial.print(*Sp);
+  Serial.print("\t"); 
+  Serial.print(*Long);
+  Serial.print("\t\t");
+  Serial.print(*Lati);
+  Serial.print("\t\t");
+  Serial.print(*Alt);
+  Serial.print("\t\t");
+  Serial.print(*Coord);
+  Serial.print("\t\t");
+  Serial.println(*GPS_Flag);
+
+}
+//----------------------------------------------------------------------------------------->
+
 void setup() {
   Serial.begin(9600);
 }
@@ -37,18 +57,7 @@ void setup() {
 void loop() {
 
   GPS_Flag = GPS_piling(&Long, &Lati, &Sp, &Alt, &Coord);
-
-
-
-  Serial.println("\nSpeed \tLongitude \tLatitude \tAltitude");
-  Serial.println("--------------------------------------------------------------");
-  Serial.print(Sp);
-  Serial.print("\t");
-  Serial.print(Long);
-  Serial.print("\t\t");
-  Serial.print(Lati);
-  Serial.print("\t\t");
-  Serial.println(Alt);
+  print_GPS(&Long, &Lati, &Sp, &Alt, &Coord, &GPS_Flag);
 
   delay(1000);
 }
