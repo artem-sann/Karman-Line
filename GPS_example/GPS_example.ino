aa43405c9a444be8355a47aa5bc80c20f1e2dd93
Artem Sannikov <artemksannikov@gmail.com>
2022-09-27 12:53:42 +0300

    GPS example

--- GPS_example/GPS_example.ino
+++ GPS_example/GPS_example.ino
@@ -1,18 +1,65 @@
+// Минимальный пример работы gps модуля платы RaceBoaard КБ INNOPOL`
 #include <AutoUpdate.h>
 #include <GPS.h>
+
+//----------------------Переменные для GPS------------------------------------------------->
+bool GPS_Flag;  //Актуальность данных (true-данные новые и не считывались)
+double Long;    //Долгота, в градусах
+double Lati;    //Широта, в градусах
+double Sp;      //Горизонтальная скорость, в метрах в секунду
+double Alt;     //ВысотаБ в метрах в секунду
+String Coord;   //Строка с координатами и высотой
+//----------------------------------------------------------------------------------------->
 
 AutoUpd_m AUpd; 
 GPS_m GPS(9600);
 
-void setup() {
+void GPS_Init(){
   GPS.StartTrack();
   AUpd.enableTimer();
   AUpd.GPSEn(30);
+} 
+
+bool GPS_piling(double *Long, double *Lati, double *Sp, double *Alt, String *Coord) {
+  
+  GPS_Flag = GPS.IsUpdated();
+  *Long = GPS.Longitude();
+  *Lati = GPS.Latitude();
+  *Sp = GPS.Speed();
+  *Alt = GPS.Altitude();
+  *Coord = GPS.LonLatAlt();
+   
+  return GPS_Flag;
+}
+
+void print_GPS(double *Long, double *Lati, double *Sp, double *Alt, String *Coord, bool *GPS_Flag) {
+
+  Serial.println(F("\nSpeed \tLongitude \tLatitude \tAltitude \tCoord \t\t\t\tGPS_Flag"));
+  Serial.println(F("---------------------------------------------------------------------------------------------------"));
+  Serial.print(*Sp);
+  Serial.print(F("\t")); 
+  Serial.print(*Long);
+  Serial.print(F("\t\t"));
+  Serial.print(*Lati);
+  Serial.print(F("\t\t"));
+  Serial.print(*Alt);
+  Serial.print(F("\t\t"));
+  Serial.print(*Coord);
+  Serial.print(F("\t\t"));
+  Serial.println(*GPS_Flag);
+
+}
+
+
+
+void setup() {
+  GPS_Init();
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(GPS.Longitude());
-  delay(100);
+  GPS_Flag = GPS_piling(&Long, &Lati, &Sp, &Alt, &Coord);
+  print_GPS(&Long, &Lati, &Sp, &Alt, &Coord, &GPS_Flag);
+  
+  delay(1000);
 }
-// Минимальный пример работы gps модуля платы RaceBoaard КБ INNOPOL`
--- SD_example/SD_example.ino
+++ SD_example/SD_example.ino
@@ -8,8 +8,14 @@
 #include <SD.h>
 
 
-AutoUpd_m AUpd; 
-
+//----------------------Переменные для GPS------------------------------------------------->
+bool GPS_Flag;  //Актуальность данных (true-данные новые и не считывались)
+double Long;    //Долгота, в градусах
+double Lati;    //Широта, в градусах
+double Sp;      //Горизонтальная скорость, в метрах в секунду
+double Alt;     //ВысотаБ в метрах в секунду
+String Coord;   //Строка с координатами и высотой
+//----------------------------------------------------------------------------------------->
 //---------------------Пины SD карты------------------------------------------------------->
 #define MISO 50
 #define MOSI 51
@@ -32,6 +38,8 @@ void SD_begin() {
 }
 
 bool SD_data_Write() {
+  long time_to_SD = millis();
+
   File dataFile = SD.open("logs.csv", FILE_WRITE);
   if (dataFile) {
     dataFile.print(time_to_SD);
@@ -47,64 +55,13 @@ bool SD_data_Write() {
 }
 
 
-//----------------------Функции для SS----------------------------------------------------->
-
-void save_event(int delay) {
-
-
-}
   
 
-//----------------------Переменные для GPS------------------------------------------------->
-bool GPS_Flag;  //Актуальность данных (true-данные новые и не считывались)
-double Long;    //Долгота, в градусах
-double Lati;    //Широта, в градусах
-double Sp;      //Горизонтальная скорость, в метрах в секунду
-double Alt;     //ВысотаБ в метрах в секунду
-String Coord;   //Строка с координатами и высотой
-//----------------------------------------------------------------------------------------->
-
-//-----------------------------Функции по работе с GPS------------------------------------->
-bool GPS_piling(double *Long, double *Lati, double *Sp, double *Alt, String *Coord) {
-  
-  
-  
-
-  GPS_Flag = GPS.IsUpdated();
-  *Long = GPS.Longitude();
-  *Lati = GPS.Latitude();
-  *Sp = GPS.Speed();
-  *Alt = GPS.Altitude();
-  *Coord = GPS.LonLatAlt();
-
-  
-  return GPS_Flag;
-}
 
 
-void print_GPS(double *Long, double *Lati, double *Sp, double *Alt, String *Coord, bool *GPS_Flag) {
-
-  Serial.println(F("\nSpeed \tLongitude \tLatitude \tAltitude \tCoord \t\t\t\tGPS_Flag"));
-  Serial.println(F("---------------------------------------------------------------------------------------------------"));
-  Serial.print(*Sp);
-  Serial.print(F("\t")); 
-  Serial.print(*Long);
-  Serial.print(F("\t\t"));
-  Serial.print(*Lati);
-  Serial.print(F("\t\t"));
-  Serial.print(*Alt);
-  Serial.print(F("\t\t"));
-  Serial.print(*Coord);
-  Serial.print(F("\t\t"));
-  Serial.println(*GPS_Flag);
-
-}
-//----------------------------------------------------------------------------------------->
 
 void setup() {
-  GPS.StartTrack();
-  AUpd.enableTimer();
-  AUpd.GPSEn(30);
+  
   Serial.begin(9600);
   SD_begin();
   if (!SD.begin(CS_pin)) {
@@ -118,9 +75,8 @@ void setup() {
 void loop() {
   
 
-  long time_to_SD = millis();
-  GPS_Flag = GPS_piling(&Long, &Lati, &Sp, &Alt, &Coord);
-  print_GPS(&Long, &Lati, &Sp, &Alt, &Coord, &GPS_Flag);
+  
+  
 
   delay(1000);
 }
