b9b64d92dafbedb7a86ef141e95f26ae12cba6f4
Artem Sannikov <artemksannikov@gmail.com>
2022-09-27 08:35:05 +0300

    Buzzer example

---
+++ Buzzer_example/Buzzer_example.ino
@@ -0,0 +1,15 @@
+//миинимальный пример работы с пищалкой на плате RaaceBoard
+#define Buzzer_Pin 4           //Пин к которому подключен бузер
+#define Buzzer_Frequency 2000  //Частота бузера в Гц
+
+#define BuzzerOn tone(Buzzer_Pin, Buzzer_Frequency)              //Активировать бузер
+#define BuzzerOnTime(x) tone(Buzzer_Pin, Buzzer_Frequency, (x))  //Активировать бузер на некоторое время в мс
+#define BuzzerOff noTone(Buzzer_Pin)                             //Деактивировать бузер
+
+void setup() {
+}
+
+void loop() {
+  BuzzerOnTime(200);
+  delay(300);
+}
